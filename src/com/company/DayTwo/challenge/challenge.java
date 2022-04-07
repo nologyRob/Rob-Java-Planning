b22e63b232ef3de0c6ad8acf8e8545284eb5e4cf
nologyRob <rob.hooper@nology.io>
2022-04-07 13:27:43 +0100

    Added challenge file

---
+++ src/com/company/DayTwo/challenge/challenge.java
@@ -0,0 +1,25 @@
+package com.company.DayTwo.challenge;
+
+public class challenge {
+
+    // So far in Java you have learnt about the following:
+    // Primitive Data Types
+    // Control Flow - if, else if, while, switch
+    // Math methods
+    // String methods
+    // System.in (User input)
+    // System.out (Console logging)
+
+
+    // Your challenge is to use this new knowledge to create 'Dice Black Jack'
+    // Dice Black Jack Rules
+        // Two Players - Person vs Computer
+            // Simple - Each player rolls the dice 5 times and the player with the highest score under or equal to 21 wins.
+            // Simple - If the score of each player goes over 21, they are bust!
+
+            // Advanced -   Each player can roll up to 5 times. They are presented their total score each time and have the decision to "hit" or "stay".
+            // Advanced -   The winner of the game is the player with the highest sore under or equal to 21
+
+    // We would like to see you use all Java learning to this point in today's challenge
+    // All code must be completed in the MAIN class.  We will organise this once we have finished the challenge.
+}
